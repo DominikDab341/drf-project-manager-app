be725d1331f9e31f2bf3a90bd06e6aa845fa6ab6
unknown <281053@student.pwr.edu.pl>
2025-12-11 22:05:33 +0100

    feat(frontend): user can change his tasks status

--- frontend/src/components/TaskDetails.jsx
+++ frontend/src/components/TaskDetails.jsx
@@ -1,90 +1,136 @@
-import { useState, useEffect } from 'react';
-import apiClient from '../api/apiClient';
-import '../css/taskDetails.css';
+  import { useState, useEffect } from 'react';
+  import apiClient from '../api/apiClient';
+  import '../css/taskDetails.css';
 
+  function TaskDetails({ taskId }) {
+    const [task, setTask] = useState(null);
+    const [comments, setComments] = useState([]);
 
-function TaskDetails({ taskId }) {
-  const [task, setTask] = useState(null);
-  const [comments, setComments] = useState([]);
+    const [commentContent, setCommentContent] = useState('');
 
-  const [commentContent, setCommentContent] = useState('');
-
-  const fetchData = async () => {
-    try {
-        const [taskRes, commentsRes] = await Promise.all([
-        apiClient.get(`/tasks/${taskId}/`),
-        apiClient.get(`/comments/?task=${taskId}`)
-      ]);
-      
-      setTask(taskRes.data);
-      setComments(commentsRes.data.results);
-    } catch (error) {
-      console.error('Błąd pobierania danych:', error);
-    }
-  };
-
-  useEffect(() => {
-    if (taskId) fetchData();
-  }, [taskId]);
-
-  const handleSendComment = async () => {
-    try {
-      await apiClient.post('/comments/', {
-        task: taskId,
-        content: commentContent,
-      });
-      setCommentContent('');
-      fetchComments();
-    } catch (error) {
-      console.error('Error sending comment:', error);
-    }
-  };
-
-
-  if (!task) return <div>Ładowanie...</div>;
-
-return (
-    <div className="task-details-container">
-      
-      <div className="task-info-section">
-        <h2>{task.title}</h2>
+    const fetchData = async () => {
+      try {
+          const [taskRes, commentsRes] = await Promise.all([
+          apiClient.get(`/tasks/${taskId}/`),
+          apiClient.get(`/comments/?task=${taskId}`)
+        ]);
         
-        <div>
-            <p><strong>Opis:</strong></p>
-            <p>{task.description}</p>
+        setTask(taskRes.data);
+        setComments(commentsRes.data.results);
+      } catch (error) {
+        console.error('Error during data fetch', error);
+      }
+    };
+
+    useEffect(() => {
+      if (taskId) fetchData();
+    }, [taskId]);
+
+    
+    const handleStatusChange = async (newStatus) => {
+      try {
+              const response = await apiClient.patch(`/tasks/${taskId}/`, {
+              status: newStatus
+          });
+          setTask(response.data);
+      } catch (error) {
+          console.error("Error during status change", error);
+          alert("Wystąpił błąd podczas zmiany statusu.");
+      }
+    };
+
+    const handleSendComment = async () => {
+      try {
+        await apiClient.post('/comments/', {
+          task: taskId,
+          content: commentContent,
+        });
+        setCommentContent('');
+        fetchData(); 
+      } catch (error) {
+        console.error('Error sending comment:', error);
+      }
+    };
+
+
+    if (!task) return <div>Ładowanie...</div>;
+
+  return (
+      <div className="task-details-container">
+        
+        <div className="task-info-section">
+          <h2>{task.title}</h2>
+          
+          <div>
+              <p><strong>Opis:</strong></p>
+              <p>{task.description}</p>
+          </div>
+
+          <p><strong>Status:</strong> {task.status}</p>
+          <p><strong>Deadline:</strong> {task.deadline || "Brak"}</p>
+          
+          <div className="status-section">
+              <p><strong>Zmień status:</strong></p>
+              <div className="status-buttons">
+        
+                  {task.status !== 'TO_DO' && (
+                      <button 
+                          className="status-btn todo"
+                          onClick={() => handleStatusChange('TO_DO')}
+                      >
+                          Do zrobienia
+                      </button>
+                  )}
+
+                  {task.status !== 'IN_PROGRESS' && (
+                      <button 
+                          className="status-btn progress"
+                          onClick={() => handleStatusChange('IN_PROGRESS')}
+                      >
+                          W trakcie
+                      </button>
+                  )}
+
+                  {task.status !== 'DONE' && (
+                      <button 
+                          className="status-btn done"
+                          onClick={() => handleStatusChange('DONE')}
+                      >
+                          Zrobione
+                      </button>
+                  )}
+                  
+              </div>
+          </div>
         </div>
 
-        <p><strong>Status:</strong> {task.status}</p>
-        <p><strong>Deadline:</strong> {task.deadline || "Brak"}</p>        
+        <div className="task-comments-section">
+          <h3>Komentarze</h3>
+          {comments.length === 0 ? (
+            <p>Brak komentarzy</p>
+          ) : (
+            comments.map((comment) => (
+              <div key={comment.id} className="task-comment">
+                <p><strong>{comment.author_name}</strong> ({new Date(comment.created_at).toLocaleString()}):</p>
+                <p>{comment.content}</p>
+              </div>
+            ))
+          )}
+
+        <textarea 
+            placeholder="Dodaj komentarz..." 
+            className="task-comment-input"
+            value ={commentContent}
+            onChange={(e) => setCommentContent(e.target.value)}
+        />
+
+        <button className="task-comment-submit-btn" onClick = {handleSendComment}>
+            Wyślij
+        </button>
+        </div>
+
       </div>
+    );
+  }
 
-      <div className="task-comments-section">
-        <h3>Komentarze</h3>
-        {comments.length === 0 ? (
-          <p>Brak komentarzy</p>
-        ) : (
-          comments.map((comment) => (
-            <div key={comment.id} className="task-comment">
-              <p><strong>{comment.author_name}</strong> ({new Date(comment.created_at).toLocaleString()}):</p>
-              <p>{comment.content}</p>
-            </div>
-          ))
-        )}
-
-      <textarea 
-          placeholder="Dodaj komentarz..." 
-          className="task-comment-input"
-          value ={commentContent}
-          onChange={(e) => setCommentContent(e.target.value)}
-      />
-
-      <button className="task-comment-submit-btn" onClick = {handleSendComment}>
-          Wyślij
-      </button>
-      </div>
-
-    </div>
-  );
-}
-
-export default TaskDetails;
+  export default TaskDetails;
